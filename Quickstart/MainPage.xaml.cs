efbf18e4453b9622b707f54b38ead58b8cb0fb39
syaifulnizamyahya <syaifulnizamyahya@gmail.com>
2016-11-24 14:44:17 +0800

    Initial commit

--- Quickstart/MainPage.xaml.cs
+++ Quickstart/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+//https://msdn.microsoft.com/en-us/windows/uwp/data-binding/data-binding-quickstart
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Quickstart
